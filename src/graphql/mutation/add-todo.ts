6d85d3979acddf848ddf3b694b72bdc5b915fd50
Posthowe <65615033+Posthowe@users.noreply.github.com>
2020-05-19 19:37:09 +0300

    Update add-todo.ts
    1. The change was made in add-todo.ts to add the items to the right collection
    
    2.  I put the  timestamp calculation within async to get the accurate timestamps for all the entries. That also solved the issue with sorting of the whole todos list

--- src/graphql/mutation/add-todo.ts
+++ src/graphql/mutation/add-todo.ts
@@ -5,16 +5,19 @@ interface Input {
 	text: string
 }
 
-const now = Date.now()
+//const now = Date.now()
+
 
 export default async ({ text }: Input): Promise<RequestResponse> => {
 	const db = await getDb()
 	const todo: Todo = {
 		text,
 		checked: false,
-		createdTimestamp: now
+		//createdTimestamp: now
+		createdTimestamp: Date.now()
 	}
-	await db.collection('todi').insertOne(todo)
+	//console.log(Date.now())
+	await db.collection('todo').insertOne(todo)
 	return {
 		status: RequestResult.SUCCESS
 	}
