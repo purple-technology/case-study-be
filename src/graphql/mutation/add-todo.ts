d9a26443a82b9de23fbd54737a095620995077ff
eunicLab <eunicennaji@hotmail.co.uk>
2020-05-23 13:50:56 +0200

    fixed bug in createdTimestamp by initializing const now inside async await function for add-todo

--- src/graphql/mutation/add-todo.ts
+++ src/graphql/mutation/add-todo.ts
@@ -5,10 +5,9 @@ interface Input {
 	text: string
 }
 
-const now = Date.now()
-
 export default async ({ text }: Input): Promise<RequestResponse> => {
 	const db = await getDb()
+	const now: number = Date.now()
 	const todo: Todo = {
 		text,
 		checked: false,
