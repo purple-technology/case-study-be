b799412e3f18d0388f575da0e9af64eb6512a04d
kxgh <fractal.kaktus@protonmail.com>
2020-06-21 20:49:24 +0200

    Adding todos & their dates fix

--- src/graphql/mutation/add-todo.ts
+++ src/graphql/mutation/add-todo.ts
@@ -5,16 +5,14 @@ interface Input {
 	text: string
 }
 
-const now = Date.now()
-
 export default async ({ text }: Input): Promise<RequestResponse> => {
 	const db = await getDb()
 	const todo: Todo = {
 		text,
 		checked: false,
-		createdTimestamp: now
+		createdTimestamp: Date.now()
 	}
-	await db.collection('todi').insertOne(todo)
+	await db.collection('todo').insertOne(todo)
 	return {
 		status: RequestResult.SUCCESS
 	}
