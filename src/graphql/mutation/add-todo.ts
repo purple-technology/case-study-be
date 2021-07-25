95ebf7865fd8faa80148dce8a0612247cd7ca7e0
domi <domi.hustinova@gmail.com>
2021-07-25 16:49:51 +0200

    fix: correct timestamp

--- src/graphql/mutation/add-todo.ts
+++ src/graphql/mutation/add-todo.ts
@@ -5,14 +5,12 @@ interface Input {
 	text: string
 }
 
-const now = Date.now()
-
 export default async ({ text }: Input): Promise<RequestResponse> => {
 	const db = await getDb()
 	const todo: Todo = {
 		text,
 		checked: false,
-		createdTimestamp: now
+		createdTimestamp: Date.now()
 	}
 	await db.collection('todo').insertOne(todo)
 	return {
